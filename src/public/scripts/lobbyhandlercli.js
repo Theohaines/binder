049057f8aa3c2a86e76321fe2c3e85f126b45692
= <=>
2024-11-20 22:49:07 +0000

    main lobby redesign

--- src/public/scripts/lobbyhandlercli.js
+++ src/public/scripts/lobbyhandlercli.js
@@ -1,6 +1,4 @@
 async function createLobby(){
-    let createLobbyCodeInput = document.getElementById("createLobbyCodeInput");
-
     fetch('/createlobby', {  
         method: 'post',
         headers: {'Content-Type': 'application/json'}
@@ -10,30 +8,44 @@ async function createLobby(){
         }
         return response.json();
     }) .then(data => {
-        let lobbyId = data.split('"').join("");
-        createLobbyCodeInput.value = "https://binder.theohaines.xyz/creator/?lobbyId=" + lobbyId;
-        createLobbyCodeInput.select();
-        createLobbyCodeInput.setSelectionRange(0, 99999);
-
-        navigator.clipboard.writeText(createLobbyCodeInput.value);
-        
-        createLobbyCodeInput.value = lobbyId;
-        document.getElementById("viewLobbyCodeInput").value = lobbyId;
-        document.getElementById("createProfileInput").value = lobbyId;
-
-        viewLobby();
+        lobbyId = data.split('"').join("");
+        prettyLayoutLobby();
     }).catch(error => {
         console.error('Fetch error:', error);
     });
 }
 
+function prettyLayoutLobby(){
+    const joinGame = document.getElementById("joinGame");
+    joinGame.style.display = "none";
+
+    const hostGame = document.getElementById("hostGame");
+    hostGame.style.width = "100%"
+
+    const gameView = document.getElementById("gameView")
+    gameView.style.display = "flex";
+
+    localStorage.setItem("lobbyId", lobbyId);
+}
+
+function joinGame(){
+    viewLobby();
+
+    const hostGame = document.getElementById("hostGame");
+    hostGame.style.display = "none";
+
+    const joinGame = document.getElementById("joinGame");
+    joinGame.style.width = "100%";
+}
+
 async function viewLobby() {
-    let viewLobbyCodeInput = document.getElementById("viewLobbyCodeInput");
+    let joinGameInput = document.getElementById("joinGameInput");
+    localStorage.setItem("lobbyId", joinGameInput.value);
 
     fetch('/viewlobby', {  
         method: 'post',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({"lobbyId" : viewLobbyCodeInput.value})
+        body: JSON.stringify({"lobbyId" : joinGameInput.value})
     }).then(response => {
         if (!response.ok) {
             alert("Status code: " + response.status + ". Did you enter a valid lobby code?");
@@ -48,7 +60,6 @@ async function viewLobby() {
 }
 
 function prettyListLobby(lobby) {
-    console.log(lobby)
     const lobbyList = document.getElementById("lobbyList");
 
     lobbyList.innerHTML = ""
@@ -74,60 +85,85 @@ function prettyListLobby(lobby) {
         profileEntryButton.setAttribute("onClick", "redirectProfileViaID("+profile.P_ID+")")
         profileEntry.appendChild(profileEntryButton);
     }
+
+    const gameView = document.getElementById("gameView")
+    gameView.style.display = "flex";
 }
 
-async function createProfileAdv(params) {
-    let createProfileInput = document.getElementById("createProfileInput");
-
-    if (createProfileInput.value.trim() == ""){ //set lobby query ref to public
-        window.redirect("/creator?lobbyId=public");
-    } else {
-        fetch('/viewlobby', {  
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({"lobbyId" : createProfileInput.value})
-        }).then(response => {
-            if (!response.ok) {
-                alert("Status code: " + response.status + ". Did you enter a valid lobby code?");
-                throw new Error('Network response was not ok');
-            }
-        }) .then(data => {
-            window.redirect("/creator?lobbyId=" + createProfileInput.value);
-        }).catch(error => {
-            console.error('Fetch error:', error);
-        });
-    }
+async function createProfileAdv() {
+    fetch('/viewlobby', {  
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({"lobbyId" : localStorage.getItem("lobbyId")})
+    }).then(response => {
+        if (!response.ok) {
+            alert("Status code: " + response.status + ". Did you enter a valid lobby code?");
+            throw new Error('Network response was not ok');
+        }
+    }) .then(data => {
+        window.redirect("/creator?lobbyId=" + localStorage.getItem("lobbyId"));
+    }).catch(error => {
+        console.error('Fetch error:', error);
+    });
 }
 
 function redirectProfileViaID(id){
-    let viewLobbyCodeInput = document.getElementById("viewLobbyCodeInput"); //We use the lobby code like a password to make sure the user is allowed to view this code ;)
-    
-    window.location.href = "/profile?id=" + id + "&lobbyId=" + viewLobbyCodeInput.value;
+    window.location.href = "/profile?id=" + id + "&lobbyId=" + localStorage.getItem("lobbyId");
 }
 
+function leaveGame(){
+    localStorage.setItem("lobbyId", "null");
+    window.location.reload();
+}
+
+// LANDING ONLY
+
 function init(){
-    if (document.getElementById("viewLobbyCodeInput")){
-        const urlParams = new URLSearchParams(window.location.search);
-        const lobbyId = urlParams.get('lobbyId');
-
-        if (lobbyId == null){
+    if (checkLanding()){
+        if (localStorage.getItem("lobbyId") == "null"){
             return;
-        }
-    
-        document.getElementById("viewLobbyCodeInput").value = lobbyId;
-        document.getElementById("createProfileInput").value = lobbyId;
-        viewLobby();
+        } else {
+            document.getElementById("joinGameInput").value = localStorage.getItem("lobbyId");
 
-        refreshLobbyView();
+            const joinGame = document.getElementById("joinGame");
+            joinGame.style.width = "100%";
+        
+            const hostGame = document.getElementById("hostGame");
+            hostGame.style.display = "none"
+        
+            const gameView = document.getElementById("gameView")
+            gameView.style.display = "flex";
+
+            viewLobby();
+            refreshLobbyView();
+        }
     }
 }
 
 function refreshLobbyView(){
     var intervalId = window.setInterval(function(){
-        if (document.getElementById("viewLobbyCodeInput").value != ""){
+        if (document.getElementById("gameView").style.display == "flex"){
             viewLobby();
         }
     }, 10000);
+}
+
+function toggleLobbyView(){
+    const lobbyView = document.getElementById("gameView");
+
+    if (lobbyView.style.display == "none"){
+        lobbyView.style.display = "flex";
+    } else {
+        lobbyView.style.display = "none";
+    }
+}
+
+function checkLanding(){
+    if (document.getElementById("gameView")){
+        return true
+    } else {
+        return false
+    }
 }
 
 init();
